b3bd757ef71b4848ea88388c58efe3a2b52787b4
ducthien97 <duongducthien1997@gmail.com>
2026-03-31 23:42:02 -0500

    Scaffold backend app

---
+++ app/src/main/java/com/app/model/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.app.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
